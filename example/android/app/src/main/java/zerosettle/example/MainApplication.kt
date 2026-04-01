2a1a261521ab82b4f0995543a9bbdba86a598800
Ryan Elliott <ryan.elliott31@gmail.com>
2026-04-01 11:07:28 -0700

    Rename example app to ZeroSettleExample
    - iOS: ZerosettleKitExample → ZeroSettleExample (project, scheme, target, directory)
    - Android: zerosettlekit.example → zerosettle.example (package name)
    - Cleaned up generated Pods/ and build/ directories

--- example/android/app/src/main/java/zerosettle/example/MainApplication.kt
+++ example/android/app/src/main/java/zerosettle/example/MainApplication.kt
@@ -1,4 +1,4 @@
-package zerosettlekit.example
+package zerosettle.example
 
 import android.app.Application
 import com.facebook.react.PackageList
--- example/ios/ZeroSettleExample/AppDelegate.swift
+++ example/ios/ZeroSettleExample/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ZerosettleKitExample",
+      withModuleName: "ZeroSettleExample",
       in: window,
       launchOptions: launchOptions
     )
